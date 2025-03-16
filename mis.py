0bb79da15923eb6a412d72a912d38b7ac7589af0
tcyang2025ab <tcyang2025ab@gmail.com>
2025-03-16 13:12:47 +0800

    Add files via upload

---
+++ mis.py
@@ -0,0 +1,6 @@
+def mis2a():
+    print("資管二A")
+
+def mis2b():
+    print("資管二B")
+
---
+++ web.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello 楊子青"
+
+@app.route("/mis")
+def course():
+    return "<h1>資訊管理導論</h1>"
+
+
+@app.route("/today")
+def today():
+    now = datetime.now()
+    return render_template("today.html", datetime = str(now))
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/welcome", methods=["GET"])
+def welcome():
+    user = request.values.get("nick")
+    return render_template("welcome.html", name=user)
+
+@app.route("/account", methods=["GET", "POST"])
+def account():
+    if request.method == "POST":
+        user = request.form["user"]
+        pwd = request.form["pwd"]
+        result = "您輸入的帳號是：" + user + "; 密碼為：" + pwd 
+        return result
+    else:
+        return render_template("account.html")
+
+if __name__ == "__main__":
+    app.run()
